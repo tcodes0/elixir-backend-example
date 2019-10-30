95b4faa0468dd24fdc4cda28d1137da72e76314d
Raphael Thomazella <thomazella9@gmail.com>
2019-10-29 21:50:14 -0300

    chore(misc): wip config

---
+++ config/config.ex
@@ -0,0 +1,10 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :minimal_server, MinimalServer.Endpoint, port: 3999
--- lib/minimal_server/endpoint.ex
+++ lib/minimal_server/endpoint.ex
@@ -1,7 +1,9 @@
 defmodule MinimalServer.Endpoint do
   use Plug.Router
+  import Logger
 
   plug(:match)
+
   plug(Plug.Parsers,
     parsers: [:json],
     pass: ["application/json"],
@@ -14,16 +16,21 @@ defmodule MinimalServer.Endpoint do
 
   plug(:dispatch)
   forward("/bot", to: MinimalServer.Router)
+
   match _ do
     send_resp(conn, 404, "Requested page not found!")
   end
+
   def child_spec(opts) do
     %{
       id: __MODULE__,
       start: {__MODULE__, :start_link, [opts]}
     }
   end
+
   def start_link(_opts) do
+    config = Application.fetch_env(:minimal_server, __MODULE__)
+    info("Starting server at http://localhost:/#{inspect(config)}")
     Plug.Cowboy.http(__MODULE__, [])
   end
 end
