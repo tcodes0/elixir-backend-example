3c9c0811af8e186b4e368d166c4b3a94aadec758
Raphael Thomazella <thomazella9@gmail.com>
2019-10-30 12:35:28 -0300

    chore(misc): redability

--- lib/minimal_server/application.ex
+++ lib/minimal_server/application.ex
@@ -2,17 +2,18 @@ defmodule MinimalServer.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-
   use Application
 
-  def start(_type, _args),
-    do: Supervisor.start_link(children(), opts())
+  def start(_type, _args) do
+    Supervisor.start_link(children(), opts())
+  end
 
   defp children do
     [
       MinimalServer.Endpoint
     ]
   end
+
   # See https://hexdocs.pm/elixir/Supervisor.html
   # for other strategies and supported options
   defp opts do
