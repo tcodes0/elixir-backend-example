5f0dd9cb73927c0b582ddb3ba2e50932774c13ca
Raphael Thomazella <thomazella9@gmail.com>
2019-11-03 19:06:11 -0300

    chore(misc): improve src script, start script

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule MinimalServer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: scripts(),
+      deps: dependencies()
     ]
   end
 
@@ -20,13 +21,19 @@ defmodule MinimalServer.MixProject do
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp dependencies do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:poison, "~> 3.0"},
       {:plug, "~> 1.6"},
       {:plug_cowboy, "~> 2.0"},
+    ]
+  end
+
+  defp scripts do
+    [
+      start: ["run --no-halt"],
     ]
   end
 end
--- scripts/env-export.sh
+++ scripts/env-export.sh
@@ -1,5 +1,6 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
+source .env
 if [ "$PORT" ]; then
   export PORT="$PORT"
 fi
